17d76de015ff2acf50f51b45797a4d70df75ddc8
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-07-11 19:55:23 +0530

    Add files via upload

---
+++ Week - 4/Exercise 4: Country REST API/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,10 @@
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        return country;
+    }
+}
