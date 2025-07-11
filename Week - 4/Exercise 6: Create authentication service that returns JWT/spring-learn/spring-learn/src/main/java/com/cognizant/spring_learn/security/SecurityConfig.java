4b9938b4d00dc70f02e9c818403b860f72f97297
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-07-11 20:11:12 +0530

    Add files via upload

---
+++ Week - 4/Exercise 6: Create authentication service that returns JWT/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.cognizant.springlearn.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()  // For development only
+                .authorizeRequests()
+                .antMatchers("/authenticate").authenticated()
+                .and()
+                .httpBasic(); // Enable Basic Auth
+    }
+}
