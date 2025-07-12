e99fa65b74298372cacf0e3c9114329b841a9182
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-07-12 22:40:07 +0530

    Add files via upload

---
+++ Week - 4/Exercise 5: Get Country by Code/spring-learning/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,12 @@
+@RestController
+public class HelloController {
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+
+  
+}
