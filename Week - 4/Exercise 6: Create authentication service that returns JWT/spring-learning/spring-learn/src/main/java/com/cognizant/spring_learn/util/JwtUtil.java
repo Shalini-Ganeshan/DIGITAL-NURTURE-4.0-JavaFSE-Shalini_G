1bcababa0d6ad6c21588c45cc16fdbdbc937eb44
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2025-07-12 22:44:42 +0530

    Add files via upload

---
+++ Week - 4/Exercise 6: Create authentication service that returns JWT/spring-learning/spring-learn/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -0,0 +1,12 @@
+public class JwtUtil {
+    private static final String SECRET = "mysecretkey";
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+}
